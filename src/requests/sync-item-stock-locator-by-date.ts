d49745e6223d8ed615dd29a2f109830947fb8104
TAN Nguyen Minh (VTI.SnP) <tan.nguyenminh@vti.com.vn>
2022-10-24 19:23:36 +0700

    Projects/avenue wmsx dev

---
+++ src/requests/sync-item-stock-locator-by-date.ts
@@ -0,0 +1,39 @@
+export class SyncItemStockLocatorByDate {
+  itemId: number;
+
+  itemName: string;
+
+  itemCode: string;
+
+  warehouseId: number;
+
+  warehouseName: string;
+
+  warehouseCode: string;
+
+  reportDate: Date;
+
+  companyId: number;
+
+  companyName: string;
+
+  companyAddress: string;
+
+  contructionId: number;
+
+  contructionName: string;
+
+  origin: string;
+
+  note: string;
+
+  locatorId: number;
+
+  locatorName: string;
+
+  locatorCode: string;
+
+  lotNumber: string;
+
+  stockQuantity: number;
+}
