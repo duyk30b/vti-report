043c651ee2e4dc2adad0396a92b6abb54ba0af5e
LONG Ngo Quang <long.ngoquang@vti.com.vn>
2022-12-04 20:09:38 +0700

    Projects/avenue wmsx dev

--- src/enums/order-type.enum.ts
+++ src/enums/order-type.enum.ts
@@ -17,3 +17,31 @@ export enum OrderTypeEnum {
   PROPOSAL = 8,
   SWIFT_LOCATOR = 9,
 }
+
+export enum WarehouseMovementTypeEnum {
+  PO_IMPORT = 0,
+  PO_EXPORT = 1,
+  PRO_IMPORT = 2,
+  PRO_EXPORT = 3,
+  SO_IMPORT = 4,
+  SO_EXPORT = 5,
+  TRANFER_IMPORT = 6,
+  TRANFER_EXPORT = 7,
+  IMO_IMPORT = 8,
+  EXO_EXPORT = 9,
+  PO_IMPORT_RETURN = 10,
+  SO_EXPORT_RETURN = 11,
+  RETURN_IMPORT = 12,
+  RETURN_EXPORT = 13,
+  RETURN_PO_ERROR = 14,
+  RETURN_SO_ERROR = 15,
+  SWIFT_FLOOR_IMPORT = 16,
+  SWIFT_FLOOR_EXPORT = 17,
+  PO_IMPORT_RECEIVE = 18,
+  PO_EXPORT_RECEIVE = 19,
+}
+
+export const MOVEMENT_TYPE_IMPORT = [
+  WarehouseMovementTypeEnum.PO_IMPORT,
+  WarehouseMovementTypeEnum.TRANFER_IMPORT,
+];
