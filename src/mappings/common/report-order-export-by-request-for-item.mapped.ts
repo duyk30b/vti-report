7ee69d21504c3568bd176db6347e06d0f17ca013
BINH Do Thien (VTI.SnP) <binh.dothien@vti.com.vn>
2023-01-12 14:43:51 +0700

    Fix/w016 format number

--- src/mappings/common/report-order-export-by-request-for-item.mapped.ts
+++ src/mappings/common/report-order-export-by-request-for-item.mapped.ts
@@ -6,6 +6,7 @@ import { ReportInfo } from './Item-inventory-mapped';
 import * as moment from 'moment';
 import { DATE_FOMAT_EXCELL, DATE_FOMAT_EXCELL_MM_DD_YY } from '@utils/constant';
 import { isEmpty } from 'lodash';
+import { readDecimal } from '@constant/common';
 
 export function getOrderExportByRequestForItemMapped(
   data: ReportOrderItem[],
@@ -26,33 +27,29 @@ export function getOrderExportByRequestForItemMapped(
       if (!prev[warehouseCode]) {
         prev[warehouseCode] = [];
       }
-      if (cur.actualQuantity) {
-        const data: ReportOrderExportByRequestForItemModel = {
-          index: 0,
-          itemCode: cur.itemCode,
-          itemName: cur.itemName,
-          warehouseExportProposals: cur.warehouseExportProposals,
-          orderCode: cur.orderCode,
-          orderCreatedAt: cur.orderCreatedAt
-            ? moment(cur.orderCreatedAt).format(DATE_FOMAT_EXCELL)
-            : '',
-          planQuantity: cur.planQuantity,
-          exportedQuantity: cur.actualQuantity,
-        };
-        prev[warehouseCode].push(data);
-      }
+      const data: ReportOrderExportByRequestForItemModel = {
+        index: 0,
+        itemCode: cur.itemCode,
+        itemName: cur.itemName,
+        warehouseExportProposals: cur.warehouseExportProposals,
+        orderCode: cur.orderCode,
+        orderCreatedAt: cur.orderCreatedAt
+          ? moment(cur.orderCreatedAt).format(DATE_FOMAT_EXCELL)
+          : '',
+        planQuantity: readDecimal(cur.planQuantity, true),
+        exportedQuantity: readDecimal(cur.actualQuantity, true),
+      };
+      prev[warehouseCode].push(data);
       return prev;
     }, {});
     const dataExcell: TableData<ReportOrderExportByRequestForItemModel>[] = [];
 
     for (const key in groupByWarehouseCode) {
-      if (!isEmpty(groupByWarehouseCode[key])) {
-        dataExcell.push({
-          warehouseCode: i18n.translate('report.WAREHOUSE_GROUP_CODE') + key,
-          data: groupByWarehouseCode[key],
-          reportType: reportType || 0,
-        });
-      }
+      dataExcell.push({
+        warehouseCode: i18n.translate('report.WAREHOUSE_GROUP_CODE') + key,
+        data: groupByWarehouseCode[key],
+        reportType: reportType || 0,
+      });
     }
 
     dataMaping.dataMapped = dataExcell || [];
