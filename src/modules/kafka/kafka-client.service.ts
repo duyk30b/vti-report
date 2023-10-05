6b88134c26bcc91ec8bb6c76405a85aac6ad2e73
DUY Ngo Van (VTI.SnP) <duy.ngovan@vti.com.vn>
2023-10-05 14:57:46 +0700

    update module kafka

--- src/modules/kafka/kafka-client.service.ts
+++ src/modules/kafka/kafka-client.service.ts
@@ -1,19 +1,35 @@
-import { Inject, Injectable, Logger } from '@nestjs/common'
+import { Inject, Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common'
 import { ClientKafka } from '@nestjs/microservices'
+import { Producer } from 'kafkajs'
 import { lastValueFrom } from 'rxjs'
 
 @Injectable()
-export class KafkaClientService {
+export class KafkaClientService implements OnModuleInit, OnModuleDestroy {
 	private readonly logger = new Logger(KafkaClientService.name)
+	private kafkaProducer: Producer
 
-	constructor(@Inject('KAFKA_SERVICE') private kafkaClient: ClientKafka) { }
+	constructor(@Inject('KAFKA_CLIENT') private kafkaClient: ClientKafka) { }
 
 	async onModuleInit() {
 		try {
-			await this.kafkaClient.connect()
+			this.kafkaProducer = await this.kafkaClient.connect()
 		} catch (error) {
 			this.logger.error(error)
 		}
+	}
+
+	async onModuleDestroy() {
+		await this.kafkaProducer.disconnect()
+	}
+
+	async emitMessage(topicName: string, content: { [P in 'data' | 'meta']?: any }, version = 1) {
+		const message = {
+			...content,
+			createTime: new Date().getTime(),
+			version,
+		}
+		const response = this.kafkaClient.emit(topicName, message)
+		return await lastValueFrom(response)
 	}
 
 	async sendMessage(topicName: string, content: { [P in 'data' | 'meta']?: any }, version = 1) {
@@ -22,7 +38,11 @@ export class KafkaClientService {
 			createTime: new Date().getTime(),
 			version,
 		}
-		const response = this.kafkaClient.emit(topicName, message)
-		return await lastValueFrom(response)
+
+		return await this.kafkaProducer.send({
+			topic: topicName,
+			messages: [{ key: 'data', value: JSON.stringify(message) }],
+			acks: -1,
+		})
 	}
 }
